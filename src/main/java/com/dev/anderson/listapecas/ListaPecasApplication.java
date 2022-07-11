4cc8cbea4d2094058785033370ffd2cbbe471c91
Anderson Matos <ams.bns2021@gmail.com>
2022-07-11 11:03:00 -0300

    init commit

---
+++ src/main/java/com/dev/anderson/listapecas/ListaPecasApplication.java
@@ -0,0 +1,13 @@
+package com.dev.anderson.listapecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaPecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaPecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/anderson/listapecas/ListaPecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.anderson.listapecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaPecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
